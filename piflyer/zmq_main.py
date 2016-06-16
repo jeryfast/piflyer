49532558f735c7fee3e934590464be08ee3b4233
Jernej <jernej.plesnar@outlook.com>
2016-06-16 10:08:29 +0200

    Code optimization with multiple processes and zeromq interprocess communication library

--- piflyer/zmq_main.py
+++ piflyer/zmq_main.py
@@ -1,6 +1,7 @@
-import os
+import os, time
 os.system("/usr/bin/firefox /home/pi/piflyer/piflyer/peer1.html &")
 os.system("python3 tornado_wss.py &")
+time.sleep(1)
 os.system("python3 zmq_sensors.py &")
 os.system("python3 zmq_commander.py &")
 os.system("python3 zmq_comm.py &")
