8986923333aa6f881c380235b147d10ce6ed732b
Jernej <jernej.plesnar@outlook.com>
2016-06-28 00:19:53 +0200

    Code optimization, general developement

--- piflyer/zmq_main.py
+++ piflyer/zmq_main.py
@@ -1,5 +1,9 @@
 import os, time
-os.system("/usr/bin/firefox /home/pi/piflyer/piflyer/peer1.html &")
+os.system("v4l2-ctl --set-fmt-video=width=720,height=480,pixelformat=H264 -p 30")
+os.system("v4l2-ctl  --set-ctrl video_bitrate=5000000")
+os.system("v4l2-ctl  --set-ctrl h264_profile=baseline")
+os.system("export DISPLAY=:99")
+os.system("sudo xvfb-run /usr/bin/firefox /home/pi/piflyer/piflyer/peer1.html &")
 os.system("python3 tornado_wss.py &")
 time.sleep(1)
 os.system("python3 zmq_sensors.py &")
